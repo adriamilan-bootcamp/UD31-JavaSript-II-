535c06a1e3cc52cc6ae6751f765a70440ac4bcdc
Adrià Milan <adria4milan@gmail.com>
2022-09-08 12:30:31 +0200

    Ejercicio 03

---
+++ Ejercicio03/js/main.js
@@ -0,0 +1,20 @@
+
+document.getElementById("p1").onclick = function() {
+    alert("Has hecho click en el párrafo 1")
+}
+
+document.getElementById("p2").onclick = function() {
+    alert("Has hecho click en el párrafo 1 de la tabla 1")
+}
+
+document.getElementById("p3").onclick = function() {
+    alert("Has hecho click en el párrafo 2 de la tabla 1")
+}
+
+document.getElementById("p4").onclick = function() {
+    alert("Has hecho click en el párrafo 1 de la tabla 2")    
+}
+
+document.getElementById("p5").onclick = function() {
+    alert("Has hecho click en el párrafo 2 de la tabla 2    ")
+}
